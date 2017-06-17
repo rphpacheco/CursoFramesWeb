1e95b482c8015dec722393caab3d79b2041b89ed
rphpacheco <rphpacheco@gmail.com>
2017-06-17 02:35:04 -0300

    include roles of tabs in application

--- angular1/app/common/components/field.js
+++ angular1/app/common/components/field.js
@@ -7,6 +7,8 @@
             grid: '@',
             placeholder: '@',
             type: '@',
+            model: '=',
+            readonly: '<',
         },
         controller: [
             'gridSystem',
@@ -18,7 +20,7 @@
             <div class="{{ $ctrl.gridClasses }}">
                 <div class="form-group">
                     <label for="{{ $ctrl.id }}">{{ $ctrl.label }}</label>
-                    <input id="{{ $ctrl.id }}" class="form-control" placeholder="{{ $ctrl.placeholder }}" type="{{ $ctrl.type }}" />
+                    <input id="{{ $ctrl.id }}" class="form-control" placeholder="{{ $ctrl.placeholder }}" type="{{ $ctrl.type }}" ng-model="$ctrl.model" ng-readonly="$ctrl.readonly" />
                 </div>
             </div>
         `
